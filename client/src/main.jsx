df59dce5aaae7cfc32014ed2d2d298155345e1ef
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-27 09:55:44 +0600

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './assets/css/style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MasterLayout from "../components/shared/MasterLayout.jsx";
+
+const HomePage = () => {
+    return (
+        <MasterLayout>
+            <h1>Page Content</h1>
+        </MasterLayout>
+    );
+};
+
+export default HomePage;
